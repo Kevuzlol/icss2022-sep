87f91b9afdb2ec21857752b7ccce5b9f0cf4c1c6
Kevuzlol <75366810+Kevuzlol@users.noreply.github.com>
2025-10-27 03:45:45 +0100

    Stack toegevoegd

---
+++ startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -0,0 +1,30 @@
+package nl.han.ica.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.EmptyStackException;
+
+public class HANStack<T> implements IHANStack<T> {
+    private List<T> stack = new ArrayList<>();
+
+    @Override
+    public void push(T value) {
+        stack.add(value);
+    }
+
+    @Override
+    public T pop() {
+        if (stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public T peek() {
+        if (stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.get(stack.size() - 1);
+    }
+}
